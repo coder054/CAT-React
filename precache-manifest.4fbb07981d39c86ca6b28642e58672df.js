d8fc7545dafe1efa160ff010cbfbd0a37aadce8a
coder054 <binhdinhgiaftu@gmail.com>
2019-02-14 19:54:23 +0700

    Updates

---
+++ precache-manifest.4fbb07981d39c86ca6b28642e58672df.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "e9f68945315f887c4105",
+    "url": "/CAT-react/static/js/runtime~main.e9f68945.js"
+  },
+  {
+    "revision": "92fc4f61270f63b46f82",
+    "url": "/CAT-react/static/js/main.92fc4f61.chunk.js"
+  },
+  {
+    "revision": "d7db5a6d6c2534f86342",
+    "url": "/CAT-react/static/js/1.d7db5a6d.chunk.js"
+  },
+  {
+    "revision": "92fc4f61270f63b46f82",
+    "url": "/CAT-react/static/css/main.5fdc0a5a.chunk.css"
+  },
+  {
+    "revision": "d7db5a6d6c2534f86342",
+    "url": "/CAT-react/static/css/1.bc739d84.chunk.css"
+  },
+  {
+    "revision": "2d5d3599a9e7dc7ff53a6a83d195d76a",
+    "url": "/CAT-react/index.html"
+  }
+];
